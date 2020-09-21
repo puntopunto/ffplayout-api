309f31b62e988775d0c73fb4ce8df52478732b8a
Jonathan Baecker <jb@pixelcrusher.de>
2020-09-21 15:02:52 +0200

    move db to mounted folder

--- ffplayout/apps/api_player/settings.py
+++ ffplayout/apps/api_player/settings.py
@@ -1,10 +1,10 @@
 import os
 
-BASE_DIR = os.path.dirname(os.path.abspath(os.path.join(__file__)))
+BASE_DIR = os.path.dirname(os.path.abspath(os.path.join(__file__, '..', '..')))
 
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'NAME': os.path.join(BASE_DIR, 'dbs', 'player.sqlite3'),
     }
 }
