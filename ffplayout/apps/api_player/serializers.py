9d46e7a1839aed2abde8f4c44e078589bb79b2fc
jb-alvarado <jb@pixelcrusher.de>
2021-03-19 12:33:43 +0100

    new log and playlist folder, when new channel is created, save instance

--- ffplayout/apps/api_player/serializers.py
+++ ffplayout/apps/api_player/serializers.py
@@ -3,26 +3,27 @@ import os
 from shutil import copyfile
 
 from apps.api_player.models import GuiSettings, MessengePresets
+from apps.api_player.utils import read_yaml, write_yaml
 from django.contrib.auth.models import User
 from rest_framework import serializers
 
 
-def create_engine_config(path, yml_config):
-    digit = os.path.basename(path).split('-')[1].split('.')[0]
+def create_engine_config(_path, yml_config):
+    suffix = os.path.basename(_path).split('-')[1].split('.')[0]
     config = configparser.ConfigParser()
     config.read('/etc/ffplayout/supervisor/conf.d/engine-001.conf')
     items = config.items('program:engine-001')
 
-    config.add_section(f'program:engine-{digit}')
+    config.add_section(f'program:engine-{suffix}')
 
     for (key, value) in items:
         if key == 'command':
             value = f'./venv/bin/python3 ffplayout.py -c {yml_config}'
-        config.set(f'program:engine-{digit}', key, value)
+        config.set(f'program:engine-{suffix}', key, value)
 
     config.remove_section('program:engine-001')
 
-    with open(path, 'w') as file:
+    with open(_path, 'w') as file:
         config.write(file)
 
 
@@ -72,8 +73,32 @@ class GuiSettingsSerializer(serializers.ModelSerializer):
             create_engine_config(validated_data['engine_service'],
                                  validated_data['playout_config'])
         if not os.path.isfile(validated_data['playout_config']):
-            copyfile('/etc/ffplayout/ffplayout-001.yml',
-                     validated_data['playout_config'])
+            suffix = os.path.basename(
+                validated_data['playout_config']).split('-')[1].split('.')[0]
+            yaml_obj = read_yaml('/etc/ffplayout/ffplayout-001.yml')
+            old_log_path = yaml_obj['logging']['log_path'].rstrip('/')
+            old_pls_path = yaml_obj['playlist']['path'].rstrip('/')
+
+            if os.path.basename(old_log_path) == 'ffplayout':
+                log_path = f'{old_log_path}/channel-{suffix}'
+            else:
+                log_path = f'{os.path.dirname(old_log_path)}/channel-{suffix}'
+
+            if os.path.basename(old_pls_path) == 'playlists':
+                play_path = f'{old_pls_path}/channel-{suffix}'
+            else:
+                play_path = f'{os.path.dirname(old_pls_path)}/channel-{suffix}'
+
+            yaml_obj['logging']['log_path'] = log_path
+            yaml_obj['playlist']['path'] = play_path
+
+            if not os.path.isdir(log_path):
+                os.makedirs(log_path, exist_ok=True)
+
+            if not os.path.isdir(play_path):
+                os.makedirs(play_path, exist_ok=True)
+
+            write_yaml(yaml_obj, validated_data['playout_config'])
 
         settings = GuiSettings.objects.create(**validated_data)
 
@@ -87,6 +112,18 @@ class GuiSettingsSerializer(serializers.ModelSerializer):
             copyfile('/etc/ffplayout/ffplayout-001.yml',
                      validated_data['playout_config'])
 
+        instance.channel = validated_data.get('channel', instance.channel)
+        instance.player_url = validated_data.get('player_url',
+                                                 instance.player_url)
+        instance.playout_config = validated_data.get('playout_config',
+                                                     instance.playout_config)
+        instance.engine_service = validated_data.get('engine_service',
+                                                     instance.engine_service)
+        instance.net_interface = validated_data.get('net_interface',
+                                                    instance.net_interface)
+        instance.media_disk = validated_data.get('media_disk',
+                                                 instance.media_disk)
+        instance.save()
         return instance
 
     class Meta:
